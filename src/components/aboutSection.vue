9a0b51d69b46f383fb49322dd1870ced508454ae
Alice Cheng <alice.cheng@machine99.thinkjam.local>
2018-11-27 17:06:32 +0000

    updating info

--- src/components/aboutSection.vue
+++ src/components/aboutSection.vue
@@ -2,10 +2,11 @@
   <div>
     <h1>about me &nbsp;<i class="fas fa-code"></i></h1>
     <div id="one"><img src="../assets/blue20.png" align="left" width="130px" height="130px">
-    <p>recent graduate from university of east anglia. currently working as a front-end developer in london. 
-      interested in video game development, full stack development, and cool web design</p>
-    <p><b>skills: </b> html, css, javascript (node, react, vue), c#, unity, mysql</p>
-    <p>creator of <a href="https://roll-for.herokuapp.com"><u>roll for initiative</u></a>, a react.js app</p>
+    <p>recent graduate from university of east anglia. currently working as a front-end developer in central london. 
+      interested in video game development, full stack development, and funky web design</p>
+    <p><b>skills: </b> html, css, javascript (node, react, vue), c#, unity, mysql, basic oop (c, c++, java)</p>
+    <p><b>creator of: </b> <a href="https://roll-for.herokuapp.com"><u>roll for initiative</u></a>, a react.js app</p>
+    <p><b>working on: </b> learning unity3d, pushing updated twitter bot to heroku</p>
   </div>
   </div>
 </template>
--- src/components/contactSection.vue
+++ src/components/contactSection.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
       <h1>contact &nbsp;<i class="far fa-comment-dots"></i></h1>
-    <p>always available at <a href="mailto:aleecheng@gmail.com"><u>aleecheng@gmail.com</u></a></p>
+    <p>always available on <a href="https://twitter.com/meiyeezy">twitter</a>, and at <a href="mailto:aleecheng@gmail.com"><u>aleecheng@gmail.com</u></a></p>
     <p>find my projects on github at <a href="https://github.com/meiyeezy"><b>meiyeezy</b></a></p>
   </div>
 </template>
