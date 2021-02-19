be74ac0e61f906cf756599adc46170c3601c55ef
Alice Cheng <alice@fuseboxgames.com>
2021-02-19 11:55:07 +0000

    updating info

--- src/components/aboutSection.vue
+++ src/components/aboutSection.vue
@@ -2,12 +2,12 @@
   <div>
     <h1>about me &nbsp;<i class="fas fa-code"></i></h1>
     <div id="one"><img src="../assets/pic2.jpg" align="left" width="130px" height="130px">
-    <p>computer science graduate from university of east anglia. currently working as a junior games designer in central london. 
-      interested in gaming, dungeons and dragons, video game development, full stack development, and funky web design</p>
-    <p><b>skills: </b> html, css, javascript (node, react, vue), c#, unity, mysql, basic oop (c, c++, java)</p>
+    <p>software engineer at fusebox games in central london. 
+      interested in gaming, dungeons and dragons, game development, game design, writing and streaming</p>
+    <p><b>skills: </b> c#, unity, javascript</p>
     <p><b>creator and owner of: </b> <a href="https://roll-for.herokuapp.com" target="_blank"><u>roll for initiative</u></a>, a react.js app; 
     <a href="https://twitter.com/critcosplaybot" target="_blank"><u>critical role cosplay bot</u></a>, a node.js twitter bot</p>
-    <p><b>working on: </b> learning unity3d, making my own games</p>
+    
   </div>
   </div>
 </template>
